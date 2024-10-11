4f11de2ec8a3292d2ad773376e4b1bd425666449
anshalshukla <shukla.anshal85@gmail.com>
2024-10-11 23:33:36 +0530

    check

--- miner/worker.go
+++ miner/worker.go
@@ -1261,7 +1261,7 @@ func (w *worker) queryFromElder() ([]string, error) {
 func (w *worker) fillTransactions(interrupt *atomic.Int32, env *environment) error {
 	// checking roll start block with current chain status is necessary as rollapp might be syncing even when the rollapp is enabled
 	// enter into the statement even if w.config.ElderRollAppEnabled is false
-	if w.config.ElderSequencerEnabled && w.config.ElderRollStartBlock <= w.chain.CurrentBlock().Number.Uint64() {
+	if w.config.ElderSequencerEnabled && w.config.ElderRollStartBlock <= w.chain.CurrentBlock().Number.Uint64()+1 {
 		resp, err := w.queryFromElder()
 		if err != nil {
 			switch err {
